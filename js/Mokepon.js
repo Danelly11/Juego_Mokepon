be33d703b83423656908acff8cb3a6eb74057cf6
danelly diaz <danellyldl@gmail.com>
2023-04-17 17:40:13 -0500

    Escuchando eventos con Javascript

--- js/Mokepon.js
+++ js/Mokepon.js
@@ -1,6 +1,12 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+}
+
 function seleccionarMascotaJugador() {
     alert("SELECCIONASTE TU MASCOTA")
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+
+
+window.addEventListener("load",iniciarJuego)
